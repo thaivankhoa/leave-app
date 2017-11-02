27316c94d4f69fb80826b7c75d2083b865e5eda3
thaivankhoa <thaivankhoa2802@gmail.com>
2017-11-02 14:52:56 +0700

    add user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
